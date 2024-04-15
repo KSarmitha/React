1e91a3b4dc4f6f6744f53fee74cf071eecbf560b
KSarmitha <ksharmitha95@gmail.com>
2024-04-15 21:30:09 +0530

    [useState] login,logout state change added

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,29 +1,35 @@
-import { LOGO_URL, PROFILE_URL } from '../utils/constants';
+import { useState } from "react";
+import { LOGO_URL, PROFILE_URL } from "../utils/constants";
 
-const logo = (
-    <img
-      className="logo"
-      src={LOGO_URL}
-      alt="Google Logo"
-    />
-  );
+const logo = <img className="logo" src={LOGO_URL} alt="Google Logo" />;
 
-const TopbarComponent = () => (
+const TopbarComponent = () => {
+  const [loginBtnName, setLoginBtnName] = useState("Login");
+
+  return (
     <div className="header">
       <div>{logo}</div>
-      <div style={{ display: "flex" }}>
+      <div
+        style={{ display: "flex", alignItems: "center", flexDirection: "row" }}
+      >
         <ul className="nav-list">
           <li className="nav-item">Home</li>
           <li className="nav-item">About Us</li>
           <li className="nav-item">Contact</li>
+          <button
+            onClick={() => {
+              loginBtnName === "Login"
+                ? setLoginBtnName("Logout")
+                : setLoginBtnName("Login");
+            }}
+          >
+            {loginBtnName}
+          </button>
         </ul>
-        <img
-          className="profile"
-          src= { PROFILE_URL }
-          alt="Profile"
-        />
+        <img className="profile" src={PROFILE_URL} alt="Profile" />
       </div>
     </div>
   );
+};
 
-  export default TopbarComponent;
+export default TopbarComponent;
