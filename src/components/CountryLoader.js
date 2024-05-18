973d9453a0f15fecb3ee48981f2a916b09d2df11
KSarmitha <ksharmitha95@gmail.com>
2024-05-18 18:51:28 +0530

    styleObj removed

--- src/components/CountryLoader.js
+++ src/components/CountryLoader.js
@@ -1,6 +1,6 @@
 const CountryLoader = () => {
     return (
-        <div style={styleObj}>
+        <div className="flex flex-col justify-center items-center">
             <div className="h-10 w-96 rounded-md animate-pulse bg-gray-400 mb-2"></div>
             <div className="h-10 w-96 rounded-md animate-pulse bg-gray-400 mb-2"></div>
             <div className="h-10 w-96 rounded-md animate-pulse bg-gray-400 mb-2"></div>
